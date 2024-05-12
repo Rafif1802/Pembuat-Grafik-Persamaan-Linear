c1fdbb6168dde675686d42c3e66d6e77469661ef
Rafif1802 <165453968+Rafif1802@users.noreply.github.com>
2024-05-12 19:39:37 +0700

    Update gs.py

--- gs.py
+++ gs.py
@@ -35,7 +35,7 @@ def main():
         """, unsafe_allow_html=True)
 
     # Menambahkan opsi baru di select box
-    menu = st.sidebar.selectbox("Menu", ('Utama', 'Perkenalan Kelompok', 'Tentang Aplikasi'))
+    menu = st.sidebar.selectbox("Menu", ('Tentang Aplikasi', 'Perkenalan Kelompok', 'Utama'))
 
     if menu == 'Utama':
         st.write('Masukkan data X dan Y dalam bentuk tabel dengan dua kolom.')
