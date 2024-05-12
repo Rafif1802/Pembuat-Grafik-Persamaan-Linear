300da60344b8a20694028db66718de46877320a8
Rafif1802 <165453968+Rafif1802@users.noreply.github.com>
2024-05-12 19:57:13 +0700

    Update gs.py

--- gs.py
+++ gs.py
@@ -83,7 +83,6 @@ def main():
     elif menu == 'Tentang Aplikasi':
         st.subheader('Tentang Aplikasi')
         st.write('Aplikasi penentu persamaan linear ini dirancang untuk memudahkan pengguna dalam melakukan perhitungan dalam penentuan persamaan linear dan mengurangi kesalahan penempatan data saat menghitung secara manual. Pengguna dapat memilih menu kalkulator untuk dapat menghitung persamaan regresi linear, nilai slope (b), nilai intersept (a), dan nilai koefisien regresi korelasi (r).')
-        st.write('<img src="https://jonmgomes.com/wp-content/uploads/2020/05/Comp_1.gif" alt="Deskripsi GIF">')
-
+        st.write('<img src="https://jonmgomes.com/wp-content/uploads/2020/05/Comp_1.gif")
 if __name__ == '__main__':
     main()
